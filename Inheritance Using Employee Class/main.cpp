63d81a342b3e5e671ddef2595280ed383ce85104
RehanGith <chrehan1007@gmail.com>
2023-11-14 20:46:42 +0500

    Employee Class Implementation

--- Inheritance Using Employee Class/main.cpp
+++ Inheritance Using Employee Class/main.cpp
@@ -12,7 +12,20 @@ public:
 	void getter() const;
 	~Employee();
 };
+ Employee::Employee(int a, string b) 
+	 : empID(a) , empName(b) {  }
+ void Employee::setter() {
+	 cout << "Enter Employee ID: ";
+	 cin >> empID;
+	 cout << "Enter Employee Name: ";
+	 getline(cin, empName);
+ }
+ void Employee::getter() const {
+	 cout << empName << " : " << empID << endl;
+ }
+ Employee::~Employee() {
 
+}
 int main() {
 
 }
