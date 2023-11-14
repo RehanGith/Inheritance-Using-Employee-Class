60d5b322f831a02cc256825e9fb94d8417044f41
RehanGith <chrehan1007@gmail.com>
2023-11-14 20:38:43 +0500

    Create An Employee Class

--- Inheritance Using Employee Class/main.cpp
+++ Inheritance Using Employee Class/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-
+class Employee {
+private:
+	int empID;
+	string empName;
+public:
+	Employee(int a = 0, string b = "");
+	void setter();
+	void getter() const;
+	~Employee();
+};
 
 int main() {
 
