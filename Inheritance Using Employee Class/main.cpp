2751b1dba4ace723e0d27ab2236ead6ffa3a69e9
RehanGith <chrehan1007@gmail.com>
2023-11-14 21:03:53 +0500

    Create Manager class using Public Inheritance From Employee

--- Inheritance Using Employee Class/main.cpp
+++ Inheritance Using Employee Class/main.cpp
@@ -21,11 +21,36 @@ public:
 	 getline(cin, empName);
  }
  void Employee::getter() const {
-	 cout << empName << " : " << empID << endl;
+	 cout <<"Name: " << empName << " : " << " ID: " << empID << endl;
  }
  Employee::~Employee() {
 
 }
+
+ class Manager : public Employee {
+ private:
+	 string position;
+	 float club_due;
+ public:
+	 Manager(int id = 0, string name = "", string dsg = "", float dues = 0.0);
+	 void setter();
+	 void getter() const;
+ };
+ Manager::Manager(int id, string name, string dsg, float dues)
+	 : Employee(id, name), position(dsg), club_due(dues) {
+
+ }
+ void Manager::setter() {
+	 Employee::setter();
+	 cout << "Enter Manager Position: ";
+	 getline(cin, position);
+	 cout << "Enter Manager Club_dues: ";
+	 cin >> club_due;
+ }
+ void Manager::getter() const {
+	 Employee::getter();
+	 cout << "Position: "<<  position << " : " << " club dues: " << club_due << endl;
+ }
 int main() {
 
 }
